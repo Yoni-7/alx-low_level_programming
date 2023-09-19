763f52fe5ceeb0fa6a9058db1c68ddb73f6cc04c
Yoni-7 <yonatanassefa15@gmail.com>
2023-09-19 04:50:49 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-main.c
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -4,11 +4,11 @@
  *  * main - check the code
  *   *
  *    * Return: Always 0.
- *     */
+ **/
 int main(void)
 {
 		char *str;
-			
+
 			str = "I do not fear computers. I fear the lack of them - Isaac Asimov"
 			_puts(str);
 			return (0);
