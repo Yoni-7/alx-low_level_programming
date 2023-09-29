95b12bd408b4bb692ae6f1c871f2e96bafd1ec6c
Yoni-7 <yonatanassefa15@gmail.com>
2023-09-29 14:45:41 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -6,12 +6,12 @@
  * Return: the length of a string.
  */
 int _strlen_recursion(char *s)
-{       
-        if (*s == '\0')
-                return (0);
-        else
-                return (1 + _strlen_recursion(s + 1));
-}       
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
 
 /**
  * comparator - compares each character of the string.
@@ -21,10 +21,10 @@ int _strlen_recursion(char *s)
  * Return: .
  */
 int comparator(char *s, int n1, int n2)
-{       
-        if (*(s + n1) == *(s + n2))
-        {       
-                if (n1 == n2 || n1 == n2 + 1)
+{
+	if (*(s + n1) == *(s + n2))
+	{
+		if (n1 == n2 || n1 == n2 + 1)
 			return (1);
 	return (0 + comparator(s, n1 + 1, n2 - 1));
 	}
